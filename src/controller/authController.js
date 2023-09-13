030b9561da363b813f365588d77622585440ac26
Koti Madduluri <kotimn@gmail.com>
2023-09-13 17:11:20 +0100

    errors optimisation

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,8 +1,9 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const asyncHandler = require("express-async-handler");
 const { User } = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
+const { httpStatusCodes } = require("../utils/errors");
 
 const { validateUserCredentilas } = require("../middleware/validator");
 
@@ -10,23 +11,26 @@ const { validateUserCredentilas } = require("../middleware/validator");
 exports.post_authenticate = asyncHandler(async (request, response) => {
     try {
         const { error } = validateUserCredentilas(request.body);
-        if (error) return response.status(400).send(error.details[0].message);
+        if (error) return response.status(httpStatusCodes.BAD_REQUEST).send(error.details[0].message);
 
         const user = await User.findOne({ email: request.body.email });
-        if (!user) return response.status(400).send("Invalid email or password");
+        if (!user) return response.status(httpStatusCodes.BAD_REQUEST).json({message:"Invalid email or password"});
 
         const validPassword = await bcrypt.compare(
             request.body.password,
             user.password
         );
         if (!validPassword)
-            return response.status(400).send("Invalid email or password");
+            return response.status(httpStatusCodes.BAD_REQUEST).json({message:"Invalid email or password"});
 
         const token = user.generateAuthToken();
-        response.json({authToken:token});
+        if(token){
+            response.status(httpStatusCodes.OK).json({ authToken: token });
+        }else{
+            response.status(httpStatusCodes.INTERNAL_SERVER).json({message:"Something went wrong"});    
+        }
     } catch (error) {
-        console.log(error);
-        response.send("An error occured");
+        response.status(httpStatusCodes.INTERNAL_SERVER).json({message:error.message});
     }
 
 });
